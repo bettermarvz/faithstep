a33236884d80702322901e19035dc25acf6a3644
MarvZ <marvs@betterteem.com>
2026-01-14 18:55:05 +0800

    fix: setup a component for useSearchParams that will be wrapped in suspense

--- app/account/setup/page.tsx
+++ app/account/setup/page.tsx
@@ -2,17 +2,22 @@
 
 import SetupForm from "@/features/account/component/SetupForm";
 import { useSearchParams } from "next/navigation";
-import React, { Suspense, use } from "react";
+import React, { Suspense } from "react";
 
-const AccountSetup = () => {
+const SetupFormWrapper = () => {
+  //useSearchParams()needs to be inside a component that's wrapped by Suspense so we made this wrapper setupFormWrapper.
   const params = useSearchParams();
-
   const code = params.get("code");
   const token = code ? String(code) : "";
+
+  return <SetupForm token={token} />;
+};
+
+const AccountSetup = () => {
   return (
     <section className="flex justify-center w-full">
       <Suspense fallback={<div>Loading...</div>}>
-        <SetupForm token={token} />
+        <SetupFormWrapper />
       </Suspense>
     </section>
   );
