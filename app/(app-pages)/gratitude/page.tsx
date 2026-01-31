e70ccc2c82a4d8351aa30a1c5e6409394cca8a1c
MarvZ <marvs@betterteem.com>
2026-01-31 21:40:26 +0800

    feat: added edditor at the bottom of post list

--- app/(app-pages)/gratitude/page.tsx
+++ app/(app-pages)/gratitude/page.tsx
@@ -3,8 +3,26 @@
 import { useGratitude } from "@/features/gratitudes/api";
 import GratitudeEditor from "@/features/gratitudes/components/GratitudeEditor";
 import GratitudeWall from "@/features/gratitudes/components/GratitudeWall";
+import React, { useEffect, useRef, useState } from "react";
 
 const Gratitude = () => {
+  const ref = useRef(null);
+  const [visibility, setVisibility] = useState(true);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setVisibility(entry.isIntersecting);
+      },
+      {
+        threshold: 0,
+      },
+    );
+    observer.observe(ref.current);
+    return () => {};
+    // return observer.disconnect();
+  }, []);
   const { data, isLoading } = useGratitude();
 
   return (
@@ -18,12 +36,17 @@ const Gratitude = () => {
             What are you thankful for today?
           </p>
         </div>
-        <GratitudeEditor charLimit={280} />
+        <div className="flex justify-center w-full" ref={ref}>
+          <GratitudeEditor charLimit={280} />
+        </div>
         {isLoading ? (
           <div>Loading All Gratitude</div>
         ) : (
           <GratitudeWall gratitudes={data} />
         )}
+        <div className="flex fixed bottom-0 justify-center w-full mb-5">
+          {!visibility && <GratitudeEditor charLimit={280} />}
+        </div>
       </div>
     </div>
   );
--- features/gratitudes/components/GratitudeWall.tsx
+++ features/gratitudes/components/GratitudeWall.tsx
@@ -1,25 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import GratitudeCard from "./GratitudeCard";
-
-const formatThisDate = (thisDate: string) => {
-  const date = new Date(thisDate);
-
-  const formatted = date
-    .toLocaleString("en-US", {
-      month: "short",
-      day: "numeric",
-      hour: "numeric",
-      minute: "2-digit",
-      hour12: true,
-    })
-    .replace(",", " at");
-  return formatted;
-};
+import { formatThisDate } from "@/lib/utils";
 
 const GratitudeWall = ({ gratitudes }: { gratitudes: any }) => {
   return (
-    <div className="grid grid-cols-3 gap-5">
+    <div className="grid grid-cols-3 gap-5 mb-52">
       {gratitudes?.map((item: any) => (
         <GratitudeCard
           key={item.id}
